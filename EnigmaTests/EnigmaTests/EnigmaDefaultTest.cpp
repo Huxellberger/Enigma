505ae51c20fc772b6c43379f4c3c557ecac50778
Jake Huxell <darkduckmovies@gmail.com>
2016-10-16 21:03:24 +0100

    Added UnitTest solution

---
+++ EnigmaTests/EnigmaTests/EnigmaDefaultTest.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) Threetee Gang, Jake Huxell 2016
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+//------------------------------------------------------------------
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+//------------------------------------------------------------------
+
+namespace EnigmaTests
+{		
+	TEST_CLASS(EnigmaTests)
+	{
+	public:
+		
+		TEST_METHOD(EnigmaTestMethod)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+//------------------------------------------------------------------
